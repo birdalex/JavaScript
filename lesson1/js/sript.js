33a96c3d21218b57dd34e84ad78a69acff8bb66b
Ivanov <ivanov.manager@gmail.com>
2018-03-26 12:47:17 +0300

    Это первый коминт

---
+++ lesson1/js/sript.js
@@ -0,0 +1,22 @@
+// var budget=prompt("Введите Ваш бюджет");
+// var shopName=prompt("Введите название магазина");
+// var shopGoods=[2];
+// for(i=0;i<=2;i++){
+// 	shopGoods[i]=prompt('Какой тип товаров будем продавать');
+// };
+// var mainList={
+// 	budget,
+// 	shopName,
+// 	shopGoods,
+// 	employers:{
+
+// 	},
+// 	open
+// };
+// document.write('Ваш бюджет на один день ='+budget/30);
+// console.log(mainList);
+
+//================================2 задание=================================
+var num=33721;
+num=(3*3*7*2*1)**3;
+console.log(num);
