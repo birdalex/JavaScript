470ab44541f7602760a991d5fa1c8da6f771ff9b
Ivanov <ivanov.manager@gmail.com>
2018-03-26 13:50:39 +0300

    Второй комминт

---
+++ lesson1/js/task1.js
@@ -0,0 +1,19 @@
+var budget=prompt("Введите Ваш бюджет");
+var shopName=prompt("Введите название магазина");
+var shopGoods=[2];
+for(i=0;i<=2;i++){
+	shopGoods[i]=prompt('Какой тип товаров будем продавать');
+};
+var mainList={
+	budget,
+	shopName,
+	shopGoods,
+	employers:{
+
+	},
+	open
+};
+document.write('Ваш бюджет на один день ='+budget/30);
+console.log(mainList);
+
+
---
+++ lesson1/js/task2.js
@@ -0,0 +1,4 @@
+var num=33721;
+num=(3*3*7*2*1)**3;
+document.write("результат = "+num);
+// console.log(num);
