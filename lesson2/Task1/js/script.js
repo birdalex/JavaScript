de8c34334fc05c67bbfd561b7849d939e8ac94f2
Ivanov <ivanov.manager@gmail.com>
2018-03-28 22:13:01 +0300

    Первое и Второе задание

--- lesson2/Task1/js/script.js
+++ lesson2/Task1/js/script.js
@@ -9,47 +9,62 @@ let mainList={
 	},
 	open:true
 };
-// for(let i=0; i<3;i++) {
-// 		let a=prompt("Введите вид товара","");
-// 		if(a!='' && (typeof(a))==='string' && a.length<50) {
-// 			if(!isNaN(a)) {
-// 				alert('Вы ввели число');
-// 				i=i-1;
-// 				continue;
-// 			}
-// 				mainList.shopGoods[i]=a;
-// 		}
-// 		else if(a===null) {
-// 			break;
-// 		}
-// 		else {
-// 			alert('Вы ничего не ввели,Повторте попытку');
-// 			i=i-1;
-// 		}	
-// };
-// alert('Ваш бюджет на один день ='+mainList.budget/30);
-// console.log(mainList);
-let count=0;
-let i;
-do{
-	
-	let i=0;
-	let a=prompt("Введите вид товара","");
+for(let i=0; i<3;i++) {
+		let a=prompt("Введите вид товара","");
 		if(a!='' && (typeof(a))==='string' && a.length<50) {
 			if(!isNaN(a)) {
 				alert('Вы ввели число');
+				i=i-1;
 				continue;
 			}
 				mainList.shopGoods[i]=a;
-				// i++;
-				count++;
 		}
 		else if(a===null) {
 			break;
 		}
 		else {
 			alert('Вы ничего не ввели,Повторте попытку');
+			i=i-1;
 		}	
-		console.log(count);
-}while(count<3);
+};
+alert('Ваш бюджет на один день ='+mainList.budget/30);
 console.log(mainList);
+// let count=0;
+// let i=0;
+// while(count<3){
+// 	let a=prompt("Введите вид товара","");
+// 		if(a!='' && (typeof(a))==='string' && a.length<50) {
+// 			if(!isNaN(a)) {
+// 				alert('Вы ввели число');
+// 				continue;
+// 			}
+// 				mainList.shopGoods[i]=a;
+// 				i++;
+// 				count++;
+// 		}
+// 		else if(a===null) {
+// 			break;
+// 		}
+// 		else {
+// 			alert('Вы ничего не ввели,Повторте попытку');
+// 		}	
+// }
+// do{
+// 	let a=prompt("Введите вид товара","");
+// 		if(a!='' && (typeof(a))==='string' && a.length<50) {
+// 			if(!isNaN(a)) {
+// 				alert('Вы ввели число');
+// 				continue;
+// 			}
+// 				mainList.shopGoods[i]=a;
+// 				i++;
+// 				count++;
+// 		}
+// 		else if(a===null) {
+// 			break;
+// 		}
+// 		else {
+// 			alert('Вы ничего не ввели,Повторте попытку');
+// 		}	
+// }while(count<3);
+// console.log(mainList);
